fc861c738348c94d7e6d40fb0e22db3a2110e604
mennA <lap tech@DESKTOP-2DQNRRA>
2024-07-10 02:52:21 +0300

    done

---
+++ API.Core/Features/UserFeatures/Commands/Handlers/UserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using API.Core.Bases;
+using API.Core.Features.UserFeatures.Commands.Models;
+using API.Core.SharedResource;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Localization;
+
+namespace API.Core.Features.UserFeatures.Commands.Handlers
+{
+    public class UserCommandHandler : Response_Handler,
+        IRequestHandler<AddUserCommand, Response<string>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly UserManager<>;
+        public UserCommandHandler(IStringLocalizer<SharedResources> stringLocalizer,
+                                  IMapper mapper) : base(stringLocalizer)
+        {
+            _mapper = mapper;
+            _stringLocalizer = stringLocalizer;
+        }
+        public Task<Response<string>> Handle(AddUserCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
